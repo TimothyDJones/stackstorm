e3312179717ae1f7fc6949a35d1d74dce00147a8
Tim Jones <tdjones74021@yahoo.com>
2020-12-03 09:04:05 -0600

    Add default message and enhance error handling.

--- hello_world/actions/hello_world_action.py
+++ hello_world/actions/hello_world_action.py
@@ -4,8 +4,8 @@ from st2common.runners.base_action import Action
 
 class HelloWorld(Action):
 
-    def run(self, name, message):
+    def run(self, name, message="Welcome to Stackstorm!"):
         print("Hello, {0}!".format(name))
-        if len(message) > 0:
+        if message and len(message) > 0:
             print(message)
             return message
